cb140650a43c7686f1e947b7cba57f2bda2ea4ed
Liudmilak <liudmilak@netvertise.com>
2018-10-18 10:20:11 +0300

    radio style with material

--- js/module-5.js
+++ js/module-5.js
@@ -1,31 +1,31 @@
-const slides = document.querySelectorAll('#slides .slide');
-const dots = document.querySelectorAll('#dotsrty .dot');
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide,3000);
-
-
-function nextSlide() {
-
-    slides[currentSlide].className = 'slide';
-    dots[currentSlide].className = 'dot';
-    currentSlide = (currentSlide+1)%slides.length;
-    dots[currentSlide].className = 'dot dotShowing';
-    slides[currentSlide].className = 'slide showing';
-}
-
-
-function selectSlider(n){
-    slides.forEach(item =>{
-            item.classList.remove('showing')
-        }
-    );
-    dots.forEach(item =>{
-            item.classList.remove('dotShowing')
-        }
-    );
-    document.getElementById(n).className = 'slide showing';
-    document.getElementById(`indicator_${n}`).className = 'dot dotShowing';
-    window.clearInterval(slideInterval);
-    currentSlide = Number(n.slice(5,n.length))-1;
-    slideInterval = setInterval(nextSlide,3000);
-   }
+// const slides = document.querySelectorAll('#slides .slide');
+// const dots = document.querySelectorAll('#dotsrty .dot');
+// let currentSlide = 0;
+// let slideInterval = setInterval(nextSlide,3000);
+//
+//
+// function nextSlide() {
+//
+//     slides[currentSlide].className = 'slide';
+//     dots[currentSlide].className = 'dot';
+//     currentSlide = (currentSlide+1)%slides.length;
+//     dots[currentSlide].className = 'dot dotShowing';
+//     slides[currentSlide].className = 'slide showing';
+// }
+//
+//
+// function selectSlider(n){
+//     slides.forEach(item =>{
+//             item.classList.remove('showing')
+//         }
+//     );
+//     dots.forEach(item =>{
+//             item.classList.remove('dotShowing')
+//         }
+//     );
+//     document.getElementById(n).className = 'slide showing';
+//     document.getElementById(`indicator_${n}`).className = 'dot dotShowing';
+//     window.clearInterval(slideInterval);
+//     currentSlide = Number(n.slice(5,n.length))-1;
+//     slideInterval = setInterval(nextSlide,3000);
+//    }
